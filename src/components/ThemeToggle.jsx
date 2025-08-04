571995e589d3f55f20deb27cba03f0eb95ee68c3
vblais12 <vblais122@gmail.com>
2025-08-03 17:33:58 -0700

    Updated theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -8,12 +8,13 @@ export const ThemeToggle = () => {
 
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
-        if (storedTheme === "dark") {
+        if (storedTheme === "dark" || !storedTheme) { // default to dark
             setIsDarkMode(true)
             document.documentElement.classList.add("dark")
+            localStorage.setItem("theme", "dark")
         } else {
-            localStorage.setItem("theme", "light")
             setIsDarkMode(false)
+            document.documentElement.classList.remove("dark")
         }
     }, [])
 
